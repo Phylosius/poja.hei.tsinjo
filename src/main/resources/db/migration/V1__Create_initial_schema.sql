6c87812f01815fb22575b729df3b63ad7ed69fcb
Phylosius <hei.phylosius@gmail.com>
2025-08-05 16:33:20 +0300

    chore(db): remove test data

--- src/main/resources/db/migration/V1__Create_initial_schema.sql
+++ src/main/resources/db/migration/V1__Create_initial_schema.sql
@@ -37,15 +37,3 @@ CREATE TABLE help (
     FOREIGN KEY (beneficiary_id) REFERENCES beneficiary(id),
     FOREIGN KEY (payment_id) REFERENCES payment(id)
 );
-
--- Preprod data
-
--- Donation
-INSERT INTO donor (id, email, full_name) VALUES ('preprod-donor-1', 'donor.one@example.com', 'Donor One');
-INSERT INTO payment (id, psp_payment_id, amount, creation_timestamp, status, method) VALUES ('preprod-payment-1', 'psp-preprod-1', 10000, '2024-05-20 10:00:00', 'SUCCEEDED', 'ORANGE_MONEY');
-INSERT INTO donation (id, donor_id, payment_id, creation_timestamp) VALUES ('preprod-donation-1', 'preprod-donor-1', 'preprod-payment-1', '2024-05-20 10:00:00');
-
--- Help
-INSERT INTO beneficiary (id, email, full_name) VALUES ('preprod-beneficiary-1', 'beneficiary.one@example.com', 'Beneficiary One');
-INSERT INTO payment (id, psp_payment_id, amount, creation_timestamp, status, method) VALUES ('preprod-payment-2', NULL, -5000, '2024-05-21 11:00:00', 'SUCCEEDED', 'ORANGE_MONEY');
-INSERT INTO help (id, beneficiary_id, payment_id, description, creation_timestamp) VALUES ('preprod-help-1', 'preprod-beneficiary-1', 'preprod-payment-2', 'Accident description for preprod.', '2024-05-21 11:00:00');
